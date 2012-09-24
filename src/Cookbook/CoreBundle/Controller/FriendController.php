fc60fca6cef7aed9132ccb5123fee03778feb8ea
edumonti <edumonti@parprpmc005980c>
2012-09-24 21:34:37 +0200

    Possibilité de modifier et supprimer un invité

--- src/Cookbook/CoreBundle/Controller/FriendController.php
+++ src/Cookbook/CoreBundle/Controller/FriendController.php
@@ -34,9 +34,8 @@ class FriendController extends Controller
             $form->bindRequest($request);
 
             if ($form->isValid()) {
-                $people = $this->get('security.context')->getToken()->getUser();
-        
-                $friend->setPeople($people);
+                
+                $friend->setPeople($usr);
                 // perform some action, such as saving the task to the database
                 $em = $this->getDoctrine()->getEntityManager();
                 $em->persist($friend);
@@ -69,6 +68,62 @@ class FriendController extends Controller
         }
     }
     
+    /**
+     * @Route("/friend/edit/{id}")
+     * @Template()
+     */
+    public function editAction($id) {
+
+        $usr= $this->get('security.context')->getToken()->getUser();
+        
+        // create a task and give it some dummy data for this example
+        $friend = $this->getDoctrine()
+                ->getRepository('CookbookCoreBundle:Friend')
+                ->find($id);
+        $form = $this->createFormBuilder($friend)
+                ->add('name','text', array(
+                'attr' => array('placeholder' => 'Nouvel invité'),
+            ))
+                ->getForm();
+        $request = $this->get('request');
+        if ($request->getMethod() == 'POST') {
+            $form->bindRequest($request);
+
+            if ($form->isValid()) {
+                
+                // perform some action, such as saving the task to the database
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($friend);
+                $em->flush();
+                if ( false !== strpos($request->headers->get('Accept'), 'text/html')) {
+                    $response = $this->redirect($this->generateUrl('friend_show', 
+                        array('id'  => $friend->getId()
+                    )));
+                }
+                
+                if (false !== strpos($request->headers->get('Accept'), 'application/json')) {
+                    $jsons = array();
+                    $jsons[]=$friend->__toArray();
+                    $response = new Response(json_encode($jsons));
+                    $response->headers->set('Content-Type', 'application/json');
+                }
+                return $response;
+            }
+        }
+        if ($request->isXmlHttpRequest()) {
+            return $this->render('CookbookCoreBundle:Friend:new.html_ajax.twig', array(
+                    'form' => $form->createView(),
+                    'user' => $usr,
+                ));
+        } else {
+            return $this->render('CookbookCoreBundle:Friend:new.html.twig', array(
+                    'form' => $form->createView(),
+                    'user' => $usr,
+                ));
+        }
+
+    }
+    
     
     /**
      * @Route("/friend/show/{id}")
@@ -92,6 +147,24 @@ class FriendController extends Controller
         $query2->setParameters(array('user_id' => $id));
         $knownRecipies= $query2->getResult();
         return $this->render('CookbookCoreBundle:Friend:show.html.twig', array('friend' => $friend, 'user' => $usr, 'mutualFriends' => $mutualfriends, 'knownRecipies' => $knownRecipies));
+    }
+    
+    /**
+     * @Route("/friend/delete/{id}")
+     * @Template()
+     */
+    public function deleteAction($id) {
+        
+        
+        $repository = $this->getDoctrine()->getRepository('CookbookCoreBundle:Friend');
+
+        $friend = $repository
+                ->find($id);
+        
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->remove($friend);
+        $em->flush();
+        return $this->redirect($this->generateUrl('friend_list'));
     }
     
     /**
